f8f1f9096884d36fea74b6a5e7e15de78ecb2cbd
Kumailabbass <126276119+Kumailabbass@users.noreply.github.com>
2023-10-27 21:08:21 +0500

    Add files via upload

---
+++ QuizApp/app/src/main/java/com/kumail/quizapp/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.kumail.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which stadium serves as the home ground for Real Madrid?",
+            "Which one is not the programming language?",
+            "Who is the current manager of Real Madrid?",
+            "What programming language is commonly used for Android app development?"
+    };
+
+    public static String choices[][] = {
+            {"Santiago Bernabéu","Camp Nou","Old Trafford","Anfield"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Zinedine Zidane","Carlo Ancelotti","Jose Mourinho","Pep Guardiola"},
+            {"Java","Python","Swift","C#"}
+    };
+
+    public static String correctAnswers[] = {
+            "Santiago Bernabéu",
+            "Notepad",
+            "Carlo Ancelotti",
+            "Java"
+    };
+
+}
